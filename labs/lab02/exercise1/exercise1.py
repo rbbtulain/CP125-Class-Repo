e9369da55802007d7e1bb3c03f018e94f4bfaa56
rbbtulain <rbbtulain@gmail.com>
2025-12-10 12:13:30 +0800

    huha

--- labs/lab02/exercise1/exercise1.py
+++ labs/lab02/exercise1/exercise1.py
@@ -2,9 +2,21 @@
 # Write your code below:
 
 def is_budget_sufficient(one_way_km, km_per_liter, price_per_liter, budget):
-    # TODO: Implement this function
-    # Calculate round trip cost and checks if within budget
-    pass
+
+    round_trip_km = one_way_km * 2
+    cost = (round_trip_km / km_per_liter) * price_per_liter
+    if budget >= cost:
+        budget = True
+    else:
+        budget = False
+    return budget
 
 # Test your code here
+
 print("Testing Road Trip Budgeter...")
+result = is_budget_sufficient(5, 2, 0.70, 100)
+
+if result:
+    print("Your money is enough for a round trip")
+else:
+    print("Your money is not enough fo a round trip")
