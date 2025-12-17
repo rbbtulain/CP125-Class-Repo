eebd2d070ebb19e1c0074a4e2bad1ea19e0abeca
rbbtulain <rbbtulain@gmail.com>
2025-12-17 13:06:29 +0800

    eakk

--- labs/lab02/exercise8/exercise8.py
+++ labs/lab02/exercise8/exercise8.py
@@ -4,12 +4,15 @@
 def calculate_bounce_height(current_height):
     """Calculate next bounce height (80% of current)."""
     # TODO: Implement this function
-    pass
+    return current_height * 0.80
 
 def is_ball_stopped(height):
     """Return True if height < 1, False otherwise."""
     # TODO: Implement this function
-    pass
+    if height < 1:
+        return True
+    else:
+        return False
 
 def simulate_bouncing_ball(start_height):
     """
@@ -17,7 +20,16 @@ def simulate_bouncing_ball(start_height):
     Returns: (bounce_count, total_distance)
     """
     # TODO: Implement using calculate_bounce_height and is_ball_stopped
-    pass
+    bounce_count = 0
+    total_distance = 0 
+
+    while is_ball_stopped(height):
+
+        bounce_count += 1
+        total_distance += 2 * height 
+
+    return bounce_count, total_distance
 
 # Test your code here
 print("Testing Bouncing Ball Simulation...")
+print(simulate_bouncing_ball(10))
