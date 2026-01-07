4379d127d7993a1f02003728958cb74de7bc09db
rbbtulain <rbbtulain@gmail.com>
2026-01-07 12:51:36 +0800

    eak

--- labs/lab04/exercise5/exercise5.py
+++ labs/lab04/exercise5/exercise5.py
@@ -2,10 +2,11 @@
 
 def find_momentum_days(prices):
     
-    pass
-
+    
+    for i inrange(1, len (prices) - 1):
 
 # Test
 prices = [100, 102, 105, 107, 106, 108, 112, 114]
 result = find_momentum_days(prices)
 print(f"Momentum days: {result}")  # Expected: [2, 5, 6]
+
