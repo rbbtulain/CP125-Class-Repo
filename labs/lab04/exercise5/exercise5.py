dab83a980c3d64e0bfe24cb612b86987889225a3
rbbtulain <rbbtulain@gmail.com>
2026-01-07 12:55:44 +0800

    eak

--- labs/lab04/exercise5/exercise5.py
+++ labs/lab04/exercise5/exercise5.py
@@ -3,7 +3,12 @@
 def find_momentum_days(prices):
     
     
-    for i inrange(1, len (prices) - 1):
+    for i in range(1, len (prices) - 1):
+        if prices[i] > prices[i - 1] and prices[i] > prices[i + 1]:
+            yield i
+
+
+
 
 # Test
 prices = [100, 102, 105, 107, 106, 108, 112, 114]
