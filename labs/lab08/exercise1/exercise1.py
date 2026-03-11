8bf71b9a002f3b4b3b8d15c0c898be45a40faa61
rbbtulain <rbbtulain@gmail.com>
2026-03-11 12:58:53 +0800

    dfbb

--- labs/lab08/exercise1/exercise1.py
+++ labs/lab08/exercise1/exercise1.py
@@ -9,6 +9,10 @@ def filter_passing_scores(input_file, output_file):
   
     passing = open(output_file, "w")
 
+    for row in lines:
+        student_id = row[0,2,4,6,8,10]
+        score = [1,3,5,7,9,11]
+
     if score >= 80:
             passing.write(f"{student_id} {score}\n")
             count += 1
